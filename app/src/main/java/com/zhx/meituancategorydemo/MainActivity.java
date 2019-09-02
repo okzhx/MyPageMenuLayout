df249ba0c85d35f07f4d23586b37d2b1324cf364
鸿轩 <1531489662@qq.com>
2019-09-02 15:54:19 +0800

    初始化项目

---
+++ app/src/main/java/com/zhx/meituancategorydemo/MainActivity.java
@@ -0,0 +1,108 @@
+package com.zhx.meituancategorydemo;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.stx.xhb.meituancategorydemo.R;
+import com.zhx.meituancategorydemo.model.ModelHomeEntrance;
+import com.zhx.meituancategorydemo.utils.ScreenUtil;
+import com.zhx.meituancategorydemo.widget.IndicatorView;
+import com.zhx.pagemenulibrary.PageMenuLayout;
+import com.zhx.pagemenulibrary.holder.AbstractHolder;
+import com.zhx.pagemenulibrary.holder.PageMenuViewHolderCreator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<ModelHomeEntrance> homeEntrances;
+    private IndicatorView entranceIndicatorView;
+    private PageMenuLayout<ModelHomeEntrance> mPageMenuLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initView();
+        init();
+    }
+
+
+    private void initView() {
+        entranceIndicatorView = findViewById(R.id.main_home_entrance_indicator);
+        mPageMenuLayout = findViewById(R.id.pagemenu);
+    }
+
+
+    private void initData() {
+        homeEntrances = new ArrayList<>();
+        homeEntrances.add(new ModelHomeEntrance("美食", R.mipmap.ic_category_0));
+        homeEntrances.add(new ModelHomeEntrance("电影", R.mipmap.ic_category_1));
+        homeEntrances.add(new ModelHomeEntrance("酒店住宿", R.mipmap.ic_category_2));
+        homeEntrances.add(new ModelHomeEntrance("生活服务", R.mipmap.ic_category_3));
+        homeEntrances.add(new ModelHomeEntrance("KTV", R.mipmap.ic_category_4));
+        homeEntrances.add(new ModelHomeEntrance("旅游", R.mipmap.ic_category_5));
+        homeEntrances.add(new ModelHomeEntrance("学习培训", R.mipmap.ic_category_6));
+        homeEntrances.add(new ModelHomeEntrance("汽车服务", R.mipmap.ic_category_7));
+        homeEntrances.add(new ModelHomeEntrance("摄影写真", R.mipmap.ic_category_8));
+        homeEntrances.add(new ModelHomeEntrance("休闲娱乐", R.mipmap.ic_category_10));
+        homeEntrances.add(new ModelHomeEntrance("丽人", R.mipmap.ic_category_11));
+        homeEntrances.add(new ModelHomeEntrance("运动健身", R.mipmap.ic_category_12));
+        homeEntrances.add(new ModelHomeEntrance("大保健", R.mipmap.ic_category_13));
+        homeEntrances.add(new ModelHomeEntrance("团购", R.mipmap.ic_category_14));
+        homeEntrances.add(new ModelHomeEntrance("景点", R.mipmap.ic_category_16));
+        homeEntrances.add(new ModelHomeEntrance("全部分类", R.mipmap.ic_category_15));
+    }
+
+    private void init() {
+        mPageMenuLayout.setPageDatas(homeEntrances, new PageMenuViewHolderCreator() {
+            @Override
+            public AbstractHolder createHolder(View itemView) {
+                return new AbstractHolder<ModelHomeEntrance>(itemView) {
+                    private TextView entranceNameTextView;
+                    private ImageView entranceIconImageView;
+
+                    @Override
+                    protected void initView(View itemView) {
+                        entranceIconImageView = itemView.findViewById(R.id.entrance_image);
+                        entranceNameTextView = itemView.findViewById(R.id.entrance_name);
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int) ((float) ScreenUtil.getScreenWidth() / 4.0f));
+                        itemView.setLayoutParams(layoutParams);
+                    }
+
+                    @Override
+                    public void bindView(RecyclerView.ViewHolder holder, final ModelHomeEntrance data, int pos) {
+                        entranceNameTextView.setText(data.getName());
+                        entranceIconImageView.setImageResource(data.getImage());
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, data.getName(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                };
+            }
+
+            @Override
+            public int getLayoutId() {
+                return R.layout.item_home_entrance;
+            }
+        });
+        entranceIndicatorView.setIndicatorCount(mPageMenuLayout.getPageCount());
+        mPageMenuLayout.setOnPageListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                entranceIndicatorView.setCurrentIndicator(position);
+            }
+        });
+    }
+}
